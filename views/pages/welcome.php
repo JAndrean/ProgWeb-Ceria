21cecb621f08bbbbf677b408c5fc8844fa273d31
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2018-04-23 18:56:31 +0700

    redirected welcome page

---
+++ views/pages/welcome.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Auto Rentals - Welcome</title>
+
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/welcome.css">
+    <script src="../../js/jquery.min.js"></script>
+    <script src="../../js/index.js"></script>
+</head>
+<body>
+    <main>
+        <div class="bgImage"></div>
+        <div class="container" id="login">
+            <div class="grid">
+                <div class="welcome">
+                    <img src="../../asset/welcome-logo.png" alt="welcome logo">
+                    <p>Click. Choose. Drive.</p>
+                </div>            
+                <div class="formBox">
+                    <form action="" method="post">
+                        <input type="text" name="userName" id="userName" placeholder="username">
+                        <input type="password" name="password" id="password" placeholder="password">
+                        <input type="submit" value="Log in" name="loginButton" id="loginButton">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
