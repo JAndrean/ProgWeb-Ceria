72c3f81078ca09b28d8277cd2a0000878351e6e2
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2018-04-23 18:54:55 +0700

    added template files

---
+++ views/footer.php
@@ -0,0 +1,29 @@
+<footer>
+    <div class="container">
+        <div>
+        </div>
+        <table class="contact">
+            <tr>
+                <td>Admin Hotline</td>
+            </tr>
+            <tr>
+                <td>phone: (0274)555-xxxx</td>
+            </tr>
+            <tr>
+                <td>e-mail: autorentail@mail.com</td>
+            </tr>
+        </table>
+        <table class="media">
+            <tr>
+                <td>Social Media</td>
+            </tr>
+            <tr>
+                <td>
+                    <i class="fa fa-facebook-square" style="font-size: 25px;"></i>
+                    <i class="fa fa-twitter-square" style="font-size: 25px;"></i>
+                    <i class="fa fa-instagram" style="font-size: 25px;"></i>		
+                </td>
+            </tr>
+        </table>
+    </div>
+</footer>
---
+++ views/header.php
@@ -0,0 +1,32 @@
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/index.js"></script>
+</head>
+<body>
+    <header>
+        <div class="topHeader">
+            <div class="container">
+                <a href="../pages/home/home.php">
+                    <div id="brand">
+                        <img src="../asset/main-logo.png" alt="main logo">
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="navbar">
+            <div class="container">
+                <ul id="navMenu">
+                    <div><img src="../asset/icons/dropdown.png" alt="dropdown" id="dropdown"></div>
+                    <li>Rentals</li>
+                    <li>Cars</li>
+                    <li>Members</li>
+                    <li>Accounts</li>
+                    <li>Logs</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div id="pushDown"></div>
